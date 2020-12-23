577d15f477917e14f60d65175e334d8f2bc39fb3
masahiro.fukuyama <be.fukuyama.m@gmail.com>
2020-12-23 10:53:51 +0900

    redux commit

--- test/first_project/src/index.js
+++ test/first_project/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+// ② 4の項目で解説
+import App from './containers/App'
+
+// ③ 次の項目で解説
+import reducer from './reducers/reducer'
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}> 
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
